b6157034585327ebd1f7e086344d2b244ef74961
109149 <109149qwe@gmail.com>
2020-12-27 14:48:43 +0400

    Add missing file argument for node profilier

--- bash/prof-node.sh
+++ bash/prof-node.sh
@@ -5,7 +5,7 @@ log="$file.log"
 
 if [[ $1 != "" ]]
 then
-  node --prof --no-logfile-per-isolate --log --logfile="$log" && node --prof-process read-file.log > "$log.txt" && less "$log.txt" && rm "$log"
+  node --prof --no-logfile-per-isolate --log --logfile="$log" "$file" && node --prof-process read-file.log > "$log.txt" && less "$log.txt" && rm "$log"
 else
   echo "Usage: ./prof-node.sh <filename>"
 fi
