dfe31a1a7e5590dea67aec10f6f4a67f9b1bd467
109149 <109149qwe@gmail.com>
2021-05-27 10:15:39 +0400

    i3lock

--- i3lock.sh
+++ i3lock.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
-icon="$HOME/images/wallpapers/space.png"
+imgBg="$HOME/images/icons/kali.logo.scaled.png"
 tmpbg='/tmp/i3lock-screen.png'
 
-# (( $# )) && { icon=$1; }
+# (( $# )) && { bg=$1; }
 
 scrot "$tmpbg"
 convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
-convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
+convert "$tmpbg" "$imgBg" -gravity center -composite -matte "$tmpbg"
 i3lock -f -e -i "$tmpbg"
 rm "$tmpbg"
+
+# convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
+# convert "$tmpbg" "$imgBg" -gravity center -composite -matte "$tmpbg"
+# i3lock -f -e -i "$tmpbg"
+
+# convert "$imgBg" -scale 75% "$tmpbg"
+# i3lock -f -e -i "$tmpbg"
+# rm "$tmpbg"
